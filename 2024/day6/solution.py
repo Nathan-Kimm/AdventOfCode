e5eee3dc0b660d7525b72d5b10043dee1b93deeb
Nathan-Kimm <nathanjkim2007@gmail.com>
2024-12-25 23:39:21 -0500

    Need to fix day 6 part 2

--- 2024/day6/solution.py
+++ 2024/day6/solution.py
@@ -1,18 +1,17 @@
-with open("test.txt", "r") as file:
+with open("input.txt", "r") as file:
     lines = file.readlines()
 
-ogarr = [list(line.strip()) for line in lines]
+arr = [list(line.strip()) for line in lines]
 
 #Find location of Guard
-for i in range(len(ogarr)):
-    for j in range(len(ogarr[0])):
-        if ogarr[i][j] == "^":
-            xog, yog = i, j
+for i in range(len(arr)):
+    for j in range(len(arr[0])):
+        if arr[i][j] == "^":
+            x, y = i, j
 
-
+obstructionseen = set()
 def runGuard(x, y, arr):
-    arr = ogarr
-    print(''.join(arr))
+    seen = set()
     iterationCount = 0
     ingrid = True
     count = 1
@@ -21,13 +20,13 @@ def runGuard(x, y, arr):
         #Guard Up
         if dir == 0:
             while(arr[x][y] != "#"):
-                arr[x][y] = "X"
+                seen.add((x,y))
                 x -= 1
                 if arr[x][y] == "#":
                     dir = 1
                     x += 1
                     break
-                elif arr[x][y] != "X":
+                elif (x,y) not in seen:
                     count += 1
                 if x < 0:
                     ingrid = False
@@ -37,13 +36,13 @@ def runGuard(x, y, arr):
         #Guard Right
         elif dir == 1:
             while(arr[x][y] != "#"):
-                arr[x][y] = "X"
+                seen.add((x,y))
                 y += 1
                 if arr[x][y] == "#":
                     dir = 2
                     y -= 1
                     break
-                elif arr[x][y] != "X": 
+                elif (x,y) not in seen: 
                     count += 1
                 if y > len(arr[0])-2:
                     ingrid = False
@@ -53,13 +52,14 @@ def runGuard(x, y, arr):
         #Guard Down
         elif dir == 2:
             while(arr[x][y] != "#"):
-                arr[x][y] = "X"
+                seen.add((x,y))
                 x += 1
                 if arr[x][y] == "#":
                     dir = -1
                     x -= 1
+
                     break
-                elif arr[x][y] != "X": 
+                elif (x,y) not in seen: 
                     count += 1
                 if x > len(arr)-2:
                     ingrid = False
@@ -69,35 +69,36 @@ def runGuard(x, y, arr):
         #Guard Left     
         elif dir == -1:
             while(arr[x][y] != "#"):
-                arr[x][y] = "X"
+                seen.add((x,y))
                 y -= 1
                 if arr[x][y] == "#":
                     dir = 0
                     y += 1
                     break
-                elif arr[x][y] != "X": 
+                elif (x,y) not in seen: 
                     count += 1 
                 if y < 0:
                     ingrid = False 
                     break
         iterationCount += 1
-        if iterationCount > 1000000:
+        if iterationCount > 10000:
+            obstructionseen.update(seen)
             return True 
     print(count)
     return False
 
 #Part 1
-runGuard(xog, yog, ogarr)
+runGuard(x, y, arr)
 
 
 # Part 2 
 obstructionCount = 0
-
-for i in range(len(ogarr)):
-    for j in range(len(ogarr[0])):
-        value = ogarr[i][j]
-        ogarr[i][j] = "#"
-        if runGuard(xog, yog, ogarr) and ogarr[i][j] != "^":
+for i in range(len(arr)):
+    for j in range(len(arr[0])):
+        value = arr[i][j]
+        if(i,j) not in obstructionseen:
+            arr[i][j] = "#"
+        if runGuard(x, y, arr) and arr[i][j] != "^":
             obstructionCount += 1
-        ogarr[i][j] = value
+        arr[i][j] = value
 print(obstructionCount)
