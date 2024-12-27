e6b9f88f804df1a6363ce74e67287bd1a1fbc12a
Nathan-Kimm <nathanjkim2007@gmail.com>
2024-12-26 23:01:30 -0500

    Finished day 5

--- 2024/day5/solution.py
+++ 2024/day5/solution.py
@@ -1,7 +1,47 @@
-with open("input.txt", "r") as file:
-    rules, updates = file.read().strip().split("\n\n")
-    rules_list = []
-    for line in rules.split("\n"):
-        a, b = line.split("|")
-        rules_list.append((int(a), int(b)))
-    updates = [list(map(int, line.split(","))) for line in updates.split("\n")]
+#Looked at hyperneutrino's solution
+file = open("input.txt")
+
+rules = []
+sum = 0
+sum2 = 0
+
+for line in file:
+    if line.isspace(): break
+    rules.append(list(map(int, line.split("|"))))
+
+cache = {}
+
+for x,y in rules:
+    cache[(x, y)] = True
+    cache[(y, x)] = False
+
+
+def is_ordered(update):
+    for i in range(len(update)):
+        for j in range(i + 1, len(update)):
+            key = (update[i], update[j])
+            if key in cache and not cache[key]:
+                return False
+    return True
+
+def sort(update):
+    for i in range(len(update)):
+        for j in range(i+1, len(update)):
+            key = (update[i], update[j])
+            if key in cache and not cache[key]:
+                key = update[j], update[i]
+# Part 1
+for line in file:
+    update = list(map(int, line.split(",")))
+    if is_ordered(update):
+        sum += update[len(update)//2]
+    else:
+        for i in range(len(update)):
+            for j in range(i+1, len(update)):
+                key = (update[i], update[j])
+                if key in cache and not cache[key]:
+                    update[i], update[j] = update[j], update[i]
+        sum2 += update[len(update)//2]
+
+print(sum)
+print(sum2)
